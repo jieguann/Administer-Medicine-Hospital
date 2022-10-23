f9c10e8f6fff0b8d3f6182a2181c59f12d8d575e
jieguann <60665347+jieguann@users.noreply.github.com>
2022-10-23 03:08:25 -0400

    Add Random Order

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private List<Transform> spawnPoints;
+    [SerializeField] private List<GameObject> spawnObjects;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i=0; i < spawnPoints.Count; i++)
+        {
+            int randomIndex = Random.Range(0, spawnObjects.Count-1);
+            Instantiate(spawnObjects[randomIndex], spawnPoints[i]);
+            spawnObjects.Remove(spawnObjects[randomIndex]);
+            
+        }
     }
 
     // Update is called once per frame
